0e95fd31f8e2998ea2facde4e47c50324db4ef12
saiurakrishna <34538779+saiurakrishna@users.noreply.github.com>
2024-07-13 12:34:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('NODEJS Project HELLO WORLD FROM K8');
+  res.send('HELLO WORLD FROM K8s Automation with CI/CD');
 });
 
 app.listen(PORT, HOST, () => {
